b415d78f084a48ecbb14072200236648d8ed528f
Humeyra Mohamed <humeyra.mohamed@argedor.com>
2022-12-09 14:06:27 +0300

    Transfer Ownership  of NFT

--- contracts/TransferOwnerShip.sol
+++ contracts/TransferOwnerShip.sol
@@ -2,7 +2,10 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "./IERC4907.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
+// import "./Wider.sol";
+
 
 interface IERC4907 {
 
@@ -10,6 +13,9 @@ interface IERC4907 {
     /// @notice Emitted when the `user` of an NFT or the `expires` of the `user` is changed
     /// The zero address for user indicates that there is no user address
     event UpdateUser(uint256 indexed tokenId, address indexed user, uint64 expires);
+    event TokenOwnershipTransferred(address indexed previousOwner, address indexed newOwner, uint256 tokenId);
+    // event TransferOwner(uint256 indexed tokenId, address newOwner);
+    //event OwnershipTransferred(address indexed previousOwner, address indexed newOwner); //adding
 
     /// @notice set the user and expires of an NFT
     /// @dev The zero address indicates there is no user
@@ -29,22 +35,30 @@ interface IERC4907 {
     /// @param tokenId The NFT to get the user expires for
     /// @return The user expires for this NFT
     function userExpires(uint256 tokenId) external view returns(uint256);
+
+  //  function transferOwnership(address newOwner) external;
+    
 }
-
-
 
 contract ERC4907 is ERC721, IERC4907 {
     struct UserInfo 
     {
         address user;   // address of user role
         uint64 expires; // unix timestamp, user expires
+        // uint256 tokenId;
     }
 
     mapping (uint256  => UserInfo) internal _users;
 
+    mapping(address => mapping (address => uint256)) allowed; //adding 
+    address public _owner; // adding 
+
+
     constructor(string memory name_, string memory symbol_)
-     ERC721(name_, symbol_) 
+     ERC721(name_, symbol_)
      {
+        _owner=msg.sender; //adding
+        emit TokenOwnershipTransferred(address(0), _msgSender(), uint256(0)); 
      }
     
     /// @notice set the user and expires of an NFT
@@ -63,6 +77,17 @@ contract ERC4907 is ERC721, IERC4907 {
         emit UpdateUser(tokenId, user, expires);
     }
 
+    function transferOwnership(address newOwner, uint256 tokenId) public virtual payable  {
+      // require(newOwner != address(0), "Ownable: new owner is the zero address");
+        require(_isApprovedOrOwner(msg.sender, tokenId));
+        ownerOf(tokenId) == newOwner;
+        _transfer(_owner, newOwner, tokenId);
+        
+    }
+
+    // require(ownerOf(1) == _owner, "Already bought");
+    // _transfer(_owner, msg.sender, 1);
+
     /// @notice Get the user address of an NFT
     /// @dev The zero address indicates that there is no user or the user is expired
     /// @param tokenId The NFT to get the user address for
@@ -72,6 +97,7 @@ contract ERC4907 is ERC721, IERC4907 {
             return  _users[tokenId].user;
         }
         else{
+        
             return ownerOf(tokenId);
             // return address(0);
         }
@@ -114,4 +140,10 @@ contract ERC4907 is ERC721, IERC4907 {
     function time() public view returns (uint256) {
         return block.timestamp;
     }
+      //adding
+    //     modifier onlyOwner()   virtual {
+    //     require(_owner == _msgSender(), "Ownable: caller is not the owner");
+    //     _;
+    // }
+
 }  
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,43 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
+//require("@nomicfoundation/hardhat-toolbox");
+/**
+ * @type import(‘hardhat/config’).HardhatUserConfig
+ */
+ require("dotenv").config();
+ require("@nomiclabs/hardhat-ethers");
+ const { API_URL, PRIVATE_KEY } = process.env;
+ module.exports = {
   solidity: {
-    version: "0.8.9",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
+        version: "0.8.17",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
+    defaultNetwork: "fuji",
+    networks: {
+      hardhat: {},
+      fuji: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`],
       },
     },
-  },
-};
+ };
+
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: {
+//     version: "0.8.9",
+//     settings: {
+//       optimizer: {
+//         enabled: true,
+//         runs: 1000,
+//       },
+//     },
+//   },
+  
+// };
 
 
